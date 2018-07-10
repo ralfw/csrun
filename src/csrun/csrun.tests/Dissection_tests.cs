464aaeaf3d6557a64da43350be120d9e1fb9ee41
Ralf Westphal <info@ralfw.de>
2018-07-10 08:11:47 +0200

    e dissection for functions

---
+++ src/csrun/csrun.tests/Dissection_tests.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using csrun.data.domain;
+using csrun.domain.compiletime;
+using NUnit.Framework;
+
+namespace csrun.tests
+{
+    [TestFixture]
+    public class Dissection_tests
+    {
+        [Test]
+        public void Main_and_functions()
+        {
+            var csrunSource = new Sourcecode
+            {
+                Filename = "main.csrun",
+                Text = new[] {
+                    "main 1",
+                    "main 2",
+                    "#functions",
+                    "f 1",
+                    "f 2",
+                    "f 3"
+                }
+            };
+
+            var result = Dissection.Dissect(csrunSource).ToArray();
+
+            Assert.AreEqual(2, result.Length);
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunMain, result[0].Section);
+            Assert.AreEqual(new[]{"main 1", "main 2"}, result[0].Text);
+            Assert.AreEqual(1, result[0].LineMappings.Mappings.First().DestinationFromLineNumber);
+            Assert.AreEqual(2, result[0].LineMappings.Mappings.First().DestinationToLineNumber);
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunFunctions, result[1].Section);
+            Assert.AreEqual(new[]{"f 1", "f 2", "f 3"}, result[1].Text);
+            Assert.AreEqual(4, result[1].LineMappings.Mappings.First().DestinationFromLineNumber);
+            Assert.AreEqual(6, result[1].LineMappings.Mappings.First().DestinationToLineNumber);
+        }
+        
+        
+        [Test]
+        public void Main_without_functions()
+        {
+            var csrunSource = new Sourcecode
+            {
+                Filename = "main.csrun",
+                Text = new[] {
+                    "main 1",
+                    "main 2",
+                }
+            };
+
+            var result = Dissection.Dissect(csrunSource).ToArray();
+
+            Assert.AreEqual(1, result.Length);
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunMain, result[0].Section);
+            Assert.AreEqual(new[]{"main 1", "main 2"}, result[0].Text);
+            Assert.AreEqual(1, result[0].LineMappings.Mappings.First().DestinationFromLineNumber);
+            Assert.AreEqual(2, result[0].LineMappings.Mappings.First().DestinationToLineNumber);
+        }
+        
+        
+        [Test]
+        public void Functions_without_main()
+        {
+            var csrunSource = new Sourcecode
+            {
+                Filename = "main.csrun",
+                Text = new[] {
+                    "#functions",
+                    "f 1",
+                    "f 2",
+                    "f 3"
+                }
+            };
+
+            var result = Dissection.Dissect(csrunSource).ToArray();
+
+            Assert.AreEqual(1, result.Length);
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunFunctions, result[0].Section);
+            Assert.AreEqual(new[]{"f 1", "f 2", "f 3"}, result[0].Text);
+            Assert.AreEqual(2, result[0].LineMappings.Mappings.First().DestinationFromLineNumber);
+            Assert.AreEqual(4, result[0].LineMappings.Mappings.First().DestinationToLineNumber);
+        }
+    }
+}
--- src/csrun/csrun/domain/compiletime/Dissection.cs
+++ src/csrun/csrun/domain/compiletime/Dissection.cs
@@ -1,18 +1,92 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using csrun.data.domain;
 
 namespace csrun.domain.compiletime
 {
-    internal class Dissection
+    internal static class Dissection
     {
         public static IEnumerable<Sourcecode> Dissect(Sourcecode csrunSource)
         {
-            yield return new Sourcecode {
+            var mainSource = Extract_main_section(csrunSource.Filename, csrunSource.Text);
+            var functionsSource = Extract_functions_section(csrunSource.Filename, csrunSource.Text);
+
+            if (IsNotEmpty(mainSource)) yield return mainSource;
+            if (IsNotEmpty(functionsSource)) yield return functionsSource;
+
+
+            bool IsNotEmpty(Sourcecode source) => source.Text.Length > 0;
+        }
+
+
+        static Sourcecode Extract_main_section(string filename, string[] text) {
+            var (mainText, fromLineNumber, toLineNumber) = Extract_main_source_text(text);
+            
+            var mainSource = new Sourcecode {
                 Section = Sourcecode.Sections.CSRunMain,
-                Filename = csrunSource.Filename,
-                Text = csrunSource.Text,
-                LineMappings = csrunSource.LineMappings
+                Filename = filename,
+                Text = mainText,
             };
+            mainSource.LineMappings.Add(new LineMapping {
+                DesinationFilename = mainSource.Filename,
+                DestinationFromLineNumber = fromLineNumber,
+                DestinationToLineNumber = toLineNumber,
+                OriginFilename = filename,
+                OriginFromLineNumber = fromLineNumber
+            });
+            return mainSource;
+        }
+
+        static (string[] text, int fromLineNumber, int toLineNumber) Extract_main_source_text(string[] text) {
+            var lines = new List<string>();
+            foreach (var l in text) {
+                if (l.Trim().StartsWith("#functions") || l.Trim().StartsWith("#test"))
+                    break;
+                lines.Add(l);
+            }
+            return (lines.ToArray(), 1, lines.Count);
+        }
+
+
+        static Sourcecode Extract_functions_section(string filename, string[] text) {
+            var (functionsText, fromLineNumber, toLineNumber) = Extract_functions_source_text(text);
+            
+            var functionsSource = new Sourcecode {
+                Section = Sourcecode.Sections.CSRunFunctions,
+                Filename = filename,
+                Text = functionsText,
+            };
+            functionsSource.LineMappings.Add(new LineMapping {
+                DesinationFilename = functionsSource.Filename,
+                DestinationFromLineNumber = fromLineNumber,
+                DestinationToLineNumber = toLineNumber,
+                OriginFilename = filename,
+                OriginFromLineNumber = fromLineNumber
+            });
+            return functionsSource;
+        }
+
+        static (string[] text, int fromLineNumber, int toLineNumber) Extract_functions_source_text(string[] text) {
+            var lines = new List<string>();
+            var fromLineNumber = 0;
+            var currLineNumber = 0;
+            
+            var inFunctions = false;
+            foreach (var l in text) {
+                currLineNumber++;
+                if (l.Trim().StartsWith("#functions")) {
+                    inFunctions = true;
+                    fromLineNumber = currLineNumber + 1;
+                }
+                else if (l.Trim().StartsWith("#test"))
+                    inFunctions = false;
+                else if (inFunctions)
+                    lines.Add(l);
+            }
+            
+            var toLineNumber = fromLineNumber + lines.Count - 1;
+
+            return (lines.ToArray(), fromLineNumber, toLineNumber);
         }
     }
 }
