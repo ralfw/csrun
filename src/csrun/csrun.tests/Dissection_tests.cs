286b22904c32dadc16019152f2a37748320fe9ee
Ralf Westphal <info@ralfw.de>
2018-07-10 12:17:34 +0200

    t test sections get dissected

--- src/csrun/csrun.tests/Dissection_tests.cs
+++ src/csrun/csrun.tests/Dissection_tests.cs
@@ -83,5 +83,59 @@ namespace csrun.tests
             Assert.AreEqual(new[]{"f 1", "f 2", "f 3"}, result[0].Text);
             Assert.AreEqual(2, result[0].OriginLineNumber);
         }
+        
+        
+        [Test]
+        public void Main_tests_and_functions()
+        {
+            var csrunSource = new Sourcecode
+            {
+                Filename = "main.csrun",
+                Text = @"m1
+m2
+#functions
+f1
+f2
+#test test 1
+t1.1
+t1.2
+#test test 2
+t2.1
+t2.2
+#test test 3
+t3.1
+t3.2
+t3.3".Split('\n')
+            };
+
+            var result = Dissection.Dissect(csrunSource).ToArray();
+
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunMain, result[0].Section);
+            Assert.AreEqual(new[]{"m1","m2"}, result[0].Text);
+            Assert.AreEqual(1, result[0].OriginLineNumber);
+
+            Assert.AreEqual(Sourcecode.Sections.CSRunFunctions, result[1].Section);
+            Assert.AreEqual(new[]{"f1","f2"}, result[1].Text);
+            Assert.AreEqual(4, result[1].OriginLineNumber);
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunTest, result[2].Section);
+            Assert.AreEqual("main.csrun#test 1", result[2].Filename);
+            Assert.AreEqual(new[]{"t1.1","t1.2"}, result[2].Text);
+            Assert.AreEqual(7, result[2].OriginLineNumber);
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunTest, result[3].Section);
+            Assert.AreEqual("main.csrun#test 2", result[3].Filename);
+            Assert.AreEqual(new[]{"t2.1","t2.2"}, result[3].Text);
+            Assert.AreEqual(10, result[3].OriginLineNumber);
+            
+            Assert.AreEqual(Sourcecode.Sections.CSRunTest, result[4].Section);
+            Assert.AreEqual("main.csrun#test 3", result[4].Filename);
+            Assert.AreEqual(new[]{"t3.1","t3.2","t3.3"}, result[4].Text);
+            Assert.AreEqual(13, result[4].OriginLineNumber);
+            
+            Assert.AreEqual(5, result.Length);
+        }
+        
     }
 }
