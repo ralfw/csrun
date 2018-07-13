f09ecb21a13fe19e809907e1eb12aaded3042701
Ralf Westphal <info@ralfw.de>
2018-07-13 18:35:58 +0200

    A test for issue #16

---
+++ src/csrun/testchild/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace testchild
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("  Child running...");
+            Console.Write("  You name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("  Welcome, {0}", name);
+        }
+    }
+}
---
+++ src/csrun/testparent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace testparent
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Parent running...");
+            var pi = Create_process_info();
+            pi.CreateNoWindow = true;
+            pi.UseShellExecute = false;
+            
+            Console.WriteLine($"-> {pi.FileName} / {pi.Arguments}");
+
+            var p = new Process {StartInfo = pi};
+            p.Start();
+            p.WaitForExit();
+            Console.WriteLine("Parent finished");
+
+            ProcessStartInfo Create_process_info() {
+                var exeFilepath = "testchild.exe";
+                return IsRunningOnMono()
+                    ? new ProcessStartInfo("mono", $"\"{exeFilepath}\"")
+                    : new ProcessStartInfo(exeFilepath, "");
+            }
+            
+            bool IsRunningOnMono() => (Type.GetType("Mono.Runtime") != null);
+        }
+    }
+}
